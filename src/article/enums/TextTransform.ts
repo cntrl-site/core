95b88d33d2c067a06c21189270d61c48e9f142f2
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-07-31 22:48:58 +0300

    added few more enums

---
+++ src/article/enums/TextTransform.ts
@@ -0,0 +1,5 @@
+export enum TextTransform {
+  None = 'none',
+  Uppercase = 'uppercase',
+  Lowercase = 'lowercase'
+}
---
+++ src/article/enums/VerticalAlign.ts
@@ -0,0 +1,5 @@
+export enum VerticalAlign {
+  Super = 'super',
+  Sub = 'sub',
+  Unset = 'unset'
+}
