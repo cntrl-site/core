ee7e4bb4089fe18bc430a643e006d5ab44f459a4
sergokiko <sergokiko@gmail.com>
2022-10-07 16:35:31 +0300

    CNTRL-471: Ability to add section mame

--- src/article/Article.schema.ts
+++ src/article/Article.schema.ts
@@ -4,6 +4,7 @@ import { Item } from './ArticleItems.schema';
 const Section = z.object({
   id: z.string().min(1),
   items: z.array(Item),
+  name: z.string().optional(),
   height: z.record(z.number().nonnegative()),
   position: z.record(z.number()),
   hidden: z.record(z.boolean())
