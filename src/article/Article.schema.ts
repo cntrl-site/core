fd8f5c18984c02136ced99f576bbe9a08bc0f546
sergokiko <sergokiko@gmail.com>
2022-11-30 17:29:52 +0200

    CNTRL-613: remove article color and height

--- src/article/Article.schema.ts
+++ src/article/Article.schema.ts
@@ -3,7 +3,5 @@ import { Section } from './Section.schema';
 
 export const ArticleSchema = z.object({
   id: z.string().min(1),
-  sections: z.array(Section),
-  height: z.record(z.number().nonnegative()),
-  color: z.record(z.nullable(z.string()))
+  sections: z.array(Section)
 });
--- src/article/Article.ts
+++ src/article/Article.ts
@@ -3,8 +3,6 @@ import { Section } from './Section';
 export interface Article {
   id: string;
   sections: Section[];
-  height: Record<LayoutIdentifier, number>;
-  color: Record<LayoutIdentifier, string | null>;
 }
 
 type LayoutIdentifier = string;
