8ec01c6dcad13d8f06abfbcde9243292070bcda4
pylypchukroman <romanpylypchuk@gmail.com>
2023-05-16 15:44:50 +0300

    CNTRL-1039: changed schema and type for keyframes

--- src/keyframes/Keyframes.schema.ts
+++ src/keyframes/Keyframes.schema.ts
@@ -7,7 +7,8 @@ const KeyframesBaseSchema = z.object({
   articleId: z.string().min(1),
   layoutId: z.string().min(1),
   itemId: z.string().min(1),
-  position: z.number().min(0)
+  position: z.number(),
+  sectionId: z.string().min(1)
 });
 
 const DimensionsKeyframeSchema = KeyframesBaseSchema.extend({
