fdb9cb6b215da947456ca1d8000f4373d0290976
sergokiko <sergokiko@gmail.com>
2023-08-07 15:43:46 +0300

    CNTRL-1200: fix comment

--- src/article/ArticleItems.schema.ts
+++ src/article/ArticleItems.schema.ts
@@ -133,7 +133,7 @@ const VimeoEmbedItemSchema = ItemBaseSchema.extend({
 const YoutubeEmbedItemSchema = ItemBaseSchema.extend({
   type: z.literal(ArticleItemType.YoutubeEmbed),
   commonParams: z.object({
-    play: z.union([z.literal('on-hover'), z.literal('on-click'), z.literal('auto')]),
+    play: z.enum(['on-hover', 'on-click', 'auto']),
     controls: z.boolean(),
     loop: z.boolean(),
     url: z.string().min(1),
